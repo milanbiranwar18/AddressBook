03a101a96d31c4b8316d999dda2bdcb113e8052e
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-05 17:40:23 +0530

    Adding Program for AddressBook UC4

--- Program.cs
+++ Program.cs
@@ -13,6 +13,12 @@
             string firstName = Console.ReadLine();
             obj.EditContact(firstName);
             AddressBookUC.PrintAddressBook();
+
+            Console.WriteLine("Enter the FirstName to Delete");
+            string firstName1 = Console.ReadLine();
+            obj.DeleteContact(firstName1);
+            AddressBookUC.PrintAddressBook();
+            Console.ReadLine();
         }
     }
 }
