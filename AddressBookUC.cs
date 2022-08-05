8984c8254caafc9e9729f02304e333f0d9b8e338
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-05 17:16:24 +0530

    Program for creating contacts in Address Book

---
+++ AddressBookUC.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBookUC
+    {
+        public static List<Contact> People = new List<Contact>();
+        public void AddNewContact()
+        {
+            Contact person = new Contact();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter ZipCode: ");
+            person.ZipCode = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            person.Email = Console.ReadLine();
+
+            People.Add(person);
+        }
+    }
+}
