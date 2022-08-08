528e9682feb3a24e247a62182815e85e7a3d0eda
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-08 20:37:56 +0530

    Adding program for multiple person in address book

--- Program.cs
+++ Program.cs
@@ -4,21 +4,24 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome to Welcome to Address Book Program");
-            AddressBookUC obj = new AddressBookUC();
-            obj.AddNewContact();
-            AddressBookUC.PrintAddressBook();
+            //Console.WriteLine("Wellcome to Welcome to Address Book Program");
+            //AddressBookUC obj = new AddressBookUC();
+            //obj.AddNewContact();
+            //AddressBookUC.PrintAddressBook();
 
-            Console.WriteLine("Enter the FirstName to Edit");
-            string firstName = Console.ReadLine();
-            obj.EditContact(firstName);
-            AddressBookUC.PrintAddressBook();
+            //Console.WriteLine("Enter the FirstName to Edit");
+            //string firstName = Console.ReadLine();
+            //obj.EditContact(firstName);
+            //AddressBookUC.PrintAddressBook();
 
-            Console.WriteLine("Enter the FirstName to Delete");
-            string firstName1 = Console.ReadLine();
-            obj.DeleteContact(firstName1);
-            AddressBookUC.PrintAddressBook();
-            Console.ReadLine();
+            //Console.WriteLine("Enter the FirstName to Delete");
+            //string firstName1 = Console.ReadLine();
+            //obj.DeleteContact(firstName1);
+            //AddressBookUC.PrintAddressBook();
+            //Console.ReadLine();
+
+            MultiplePerson multiplePerson = new MultiplePerson();
+            multiplePerson.AddMultipleContact1();
         }
     }
 }
