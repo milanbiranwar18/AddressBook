194e23f870aebdf42d4f97219638636d7fe73a6e
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-08 20:35:14 +0530

    Program for adding multiple person in address book

---
+++ MultiplePerson.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    public class MultiplePerson
+    {
+        AddressBookUC ObjAdd = new AddressBookUC();
+        public void AddMultipleContact1()
+        {
+            Console.WriteLine("Enter the Number of Persons to add");
+            int Num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= Num; i++)
+            {
+                ObjAdd.AddNewContact();
+            }
+        }
+    }
+}
