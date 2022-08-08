5b4347f705d03b1bd004ae9898f53422f0aca9ac
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-08 22:00:29 +0530

    Adding Program for multiple address book

---
+++ MultipleAddressBook.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddressBook;
+
+namespace AddressBook
+{
+    public class MultipleAddressBook
+    {
+        Dictionary<string, AddressBookUC> MultiDict = new Dictionary<string, AddressBookUC>();
+        public void AddAddressBook()
+        {
+
+            Console.WriteLine("How many ADDRESSBOOK, You want to create ");
+            int BookNum = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= BookNum; i++)
+            {
+                AddressBookUC ObjAdd = new AddressBookUC();
+
+                Console.WriteLine("What name you want to give to address book");
+                
+                string Bookname = Console.ReadLine();
+                MultiDict.Add(Bookname, ObjAdd);
+
+                
+                ObjAdd.AddMultipleContact1();
+
+            }
+            Console.WriteLine("AddressBook added successfully");
+
+        }
+    }
+}
